2a8b7cc17a04b6182506f4e2153ac9354c2d46f8
ajay kumar <ajaybharwal0786@gmail.com>
2022-09-26 13:33:54 -0400

    your commit comment

---
+++ bash/sysinfo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#LAB 1 SYSTEM INFO QUIZ
+
+echo 'This is a system info Script'
+
+hostname -A #used for system’s fully-qualified domain name 
+
+hostnamectl # used to  show system hostname , operating system name and version 
+
+hostname -I # used to show ip address of the machine except 127
+df -h / #this command used for show available space in root folder as a human readable form
+
