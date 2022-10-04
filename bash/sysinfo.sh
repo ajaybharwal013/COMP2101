bec90b9c7bea6d077b00f61164d4c21ecc6e36b4
ajay kumar <ajaybharwal0786@gmail.com>
2022-10-04 13:48:06 -0400

    lab2

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-#LAB 1 SYSTEM INFO QUIZ
+#LAB 2 SYSTEM INFO
+echo Report for myvm
+echo -e  '-------------------\n-----------------'
+echo "FQDN: $(hostname -A)" #used for system’s fully-qualified domain name 
+echo "Hostname: $(hostname)" #used for hostname 
+echo "Operating System name and version:$(hostnamectl)" # used to  show system hostname , operating system name and version 
 
-echo 'This is a system info Script'
+echo "Operating System name and version:$(lsb_release -a)" # used to  show system hostname , operating system name and version
+# echo "System details:$(hostnamectl)" # used to  show system hostname , operating system name and version
 
-hostname -A #used for system’s fully-qualified domain name 
-
-hostnamectl # used to  show system hostname , operating system name and version 
-
-hostname -I # used to show ip address of the machine except 127
-df -h / #this command used for show available space in root folder as a human readable form
+echo "Root FileSystem Free Space: $(df -h /)" #this command used for show available space in root folder as a human readable form
+echo -e  '------------------- \n------------------'
 
